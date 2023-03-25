2e5ec1ae88ee26a62d61e7378aef552c69d9b127
salihcancakar <salihxcan@hotmail.com>
2023-03-25 11:10:47 +0300

    Components

---
+++ odev-6/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <webHeader></webHeader>
+    <hr />
+    <serverStatus></serverStatus>
+    <hr />
+    <webFooter></webFooter>
+  </div>
+</template>
+
+<script>
+import Header from "./header.vue";
+import ServerStatus from "./serverStatus.vue";
+import Footer from "./footer.vue";
+
+export default {
+  components: {
+    webHeader: Header,
+    serverStatus: ServerStatus,
+    webFooter: Footer
+  }
+};
+</script>
+
+<style></style>
